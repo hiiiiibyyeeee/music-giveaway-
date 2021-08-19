92849be2c54d8f035d5504fd29d9e80ab31d4735
drANO <82681184+DrANO404@users.noreply.github.com>
2021-08-20 02:28:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODAyOTk1MzUzMjA5MDEyMjM0.YA3Vsw.kbVeAQamauwN6u1OSoAkHK_2FEk');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
